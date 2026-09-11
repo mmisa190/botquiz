8faa815ea3e9027025ac62ffdef9f71f04bea296
mmisa190 <m.misa.18.190@gmail.com>
2026-09-11 21:57:16 +0330

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,25 @@
+import os
 
+from bale import Bot, Message
+from bale.handlers import CommandHandler
+
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+
+
+@bot.handle(CommandHandler("start"))
+async def start_command(message: Message):
+    await message.reply(
+        "🎌 سلام! به Anime Quiz خوش اومدی!\n\n"
+        "برای شروع کوییز آماده‌ای؟ 😎"
+    )
+
+
+@bot.listen("on_ready")
+async def on_ready():
+    print("🤖 Anime Quiz Bot is ready!")
+
+
+bot.run()
